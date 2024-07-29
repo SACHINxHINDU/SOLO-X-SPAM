016b7a7e1d18dde2ba2ba39c4b681898ee6a89d2
SACHINxHINDU <sachinxxsanatani@gmail.com>
2024-07-29 16:09:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,8 +16,8 @@ logging.basicConfig(
 API_ID = 18136872
 API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", "solo-x-spammmm")
-HEROKU_API_KEY = getenv("HEROKU_API_KEY", "91f976ce-9ba3-4b3a-8e69-b8eb5c3a64e8")
+#HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", "solo-x-spammmm")
+#HEROKU_API_KEY = getenv("HEROKU_API_KEY", "91f976ce-9ba3-4b3a-8e69-b8eb5c3a64e8")
 BOT_TOKEN = getenv("BOT_TOKEN", default="7439140731:AAHk4e5ZmzdPiRt3LJ5p7rUt1nD2xwbF2UQ")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://SachinSanatani:SACHINxSANATANI@sanatani.bnmsfbd.mongodb.net/?retryWrites=true&w=majority&appName=Sanatani")
 
