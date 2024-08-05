6ccf4456665a1b48ba075a971fa5d2572b3182a9
SACHINxHINDU <sachinxxsanatani@gmail.com>
2024-08-05 14:15:15 +0530

    Update clone.py

--- JARVIS/modules/clone.py
+++ JARVIS/modules/clone.py
@@ -1,13 +1,9 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 import re
 import logging
-from pymongo import MongoClient
-from pyrogram import Client, filters
-from pyrogram.types import Message
-from pyrogram.errors.exceptions.bad_request_400 import AccessTokenExpired, AccessTokenInvalid
+from telethon import MongoClient
+from telethon import Client, filters
+from telethon.types import Message
+from telethon.errors.exceptions.bad_request_400 import AccessTokenExpired, AccessTokenInvalid
 from config import API_ID, API_HASH, MONGO_DB_URI, MONGO_DB_NAME, CLONE_MODE
 
 mongo_client = MongoClient(MONGO_DB_URI)
